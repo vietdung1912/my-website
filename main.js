73654a19915cd5e3b02f38f3ff260f62b5b3cbb3
vietdung1912 <pham.viet.dung.19122000@gmail.com>
2022-07-10 22:26:14 +0700

    Commit source 2

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-function activeTag(e) {
+function activeTag(element) {
     var type = document.querySelectorAll('.action')
     for (var index of type) {
         index.classList.remove('active')
     }
-    e.classList.add('active')
+    element.classList.add('active')
+}
+
+function handleDelete(element) {
+    var taskItem = element.parentElement;
+    if (confirm("Are you sure?") == true){
+        taskItem.remove()
+    }
+}
+
+function handleAddTask() {
+    var input = document.querySelector(".input-task")
+    console.log(input.value)
+    if (input.value === "") {
+        console.log("Empty input.")
+    }
+    else {
+        var listItem = document.querySelector('.list-item')
+        
+    }
 }
