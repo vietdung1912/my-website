81a4b9b8bb84bf300a8fc7959819182c718a6ca1
vietdung1912 <pham.viet.dung.19122000@gmail.com>
2022-07-11 10:48:51 +0700

    Commit source 3

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+var listTask = [
+    'Task 1',
+    'Task 2',
+    'Task 3',
+    'Task 4',
+    'Task 5',
+    'Task 6',
+    'Task 7',
+    'Task 5',
+    'Task 6',
+    'Task 7',
+    'Task 5',
+    'Task 6',
+    'Task 7 Task 7 Task 7 Task 7 Task 7 Task 7 Task 7 Task 7',
+]
+
+function showNumberTask(listTask) {
+    var numTask = document.querySelector('.list-info')
+    numTask.innerHTML = `${listTask.length} tasks.`
+}
+
+function searchTask(element) {
+    var result = listTask.filter(task => task == element.value.trim())
+    renderTask(result.length === 0 ? listTask : result)
+    
+}
+
+function renderTask(listTask) {
+    var htmls = document.querySelector('.list-item')
+    var list = listTask.map(task => {
+        return `
+            <div class='item'>
+                <div class="item-info">${task}</div>
+                <div class="delete-btn" onclick="handleDelete(this)">Delete</div>
+            </div>
+        `
+    })
+    showNumberTask(listTask)
+    htmls.innerHTML = list.join('')
+}
+
 function activeTag(element) {
     var type = document.querySelectorAll('.action')
     for (var index of type) {
@@ -10,6 +51,7 @@ function handleDelete(element) {
     var taskItem = element.parentElement;
     if (confirm("Are you sure?") == true){
         taskItem.remove()
+        showNumberTask(listTask)
     }
 }
 
@@ -20,7 +62,10 @@ function handleAddTask() {
         console.log("Empty input.")
     }
     else {
-        var listItem = document.querySelector('.list-item')
-        
+        listTask.push(input.value)
+        input.value = ''
+        renderTask(listTask)
     }
 }
+
+renderTask(listTask)
